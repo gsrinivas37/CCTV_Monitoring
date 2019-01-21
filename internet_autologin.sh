0cd5915fae06f0f12283f43742de70e5af3326ed
Srinivas Gudla <gsrinivas37@gmail.com>
2019-01-21 06:24:22 +0530

    Add internet login check

--- internet_autologin.sh
+++ internet_autologin.sh
@@ -2,5 +2,9 @@
 status=`wget -O - --server-response http://login.hireachbroadband.com  2>&1 | awk '/^  Location:/{print $2}'`
 if [[ "$status" == *login ]]; then
     wget  -O - --quiet --post-data 'user=g_narahari&password=123456' http://login.hireachbroadband.com/login
-    echo "Auto logged on $(date)" >> /home/pi/www/logs/internet_autologin/log.txt
+    status=`wget -O - --server-response http://login.hireachbroadband.com  2>&1 | awk '/^  Location:/{print $2}'`
+    if [[ "$status" == *login ]]; then
+	echo "No internet at $(date)"
+    else
+        echo "Auto logged on $(date)" >> /home/pi/www/logs/internet_autologin/log.txt
 fi
