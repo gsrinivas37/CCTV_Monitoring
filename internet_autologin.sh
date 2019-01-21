38f84c06b486cb97e7ace63318c21f4c2cb6e1e5
Srinivas Gudla <gsrinivas37@gmail.com>
2019-01-21 07:04:41 +0530

    better check for internet auto login

--- internet_autologin.sh
+++ internet_autologin.sh
@@ -4,7 +4,12 @@ if [[ "$status" == *login ]]; then
     wget  -O - --quiet --post-data 'user=g_narahari&password=123456' http://login.hireachbroadband.com/login
     status=`wget -O - --server-response http://login.hireachbroadband.com  2>&1 | awk '/^  Location:/{print $2}'`
     if [[ "$status" == *login ]]; then
-	echo "No internet at $(date)"
+	if [ ! -f /home/pi/www/logs/internet_autologin/nonet ]; then
+	    echo "No internet at $(date)" >> /home/pi/www/logs/internet_autologin/log.txt
+	    touch /home/pi/www/logs/internet_autologin/nonet
+	fi
     else
         echo "Auto logged on $(date)" >> /home/pi/www/logs/internet_autologin/log.txt
+	rm /home/pi/www/logs/internet_autologin/nonet
+    fi
 fi
