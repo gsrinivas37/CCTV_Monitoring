43475363aefa634c2d8c26bc86faf8d154e991ac
Srinivas Gudla <gsrinivas37@gmail.com>
2019-07-13 17:17:58 -0400

    .

--- backup.py
+++ backup.py
@@ -7,7 +7,6 @@ def backup(date):
     hours = get_sub_dirs(root_dir)
     for hour_dir in hours:
         ensure_dir_exists(os.path.join(target_dir,hour_dir))
-        images_list = get_files(os.path.join(root_dir,hour_dir),"jpg")
         person_dir = os.path.join(root_dir,hour_dir,"persons")
         thumbnail_dir = os.path.join(root_dir,hour_dir,"thumbnails")
         shutil.copytree(thumbnail_dir,os.path.join(target_dir,hour_dir,"thumbnails"))
@@ -15,9 +14,5 @@ def backup(date):
             person_list = get_files(person_dir,"jpg")
             for p in person_list:
                 print("Copy full image:"+p)
-                images_list.remove(p)
-        for img in images_list:
-            print("Copy thumbnail image:"+img)
-
 
 backup("2019-07-13")
